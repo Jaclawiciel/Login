34a764f51522c85c96b3e22ca4ea05f46bf95667
Jacek Gałka <jacgalka@icloud.com>
2017-10-06 16:18:55 +0200

    Segues programmatically

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    //MARK: Actions
+    
+    @IBAction func forgotButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let sender = sender as? UIButton else { return }
+        var titleToSet: String? = nil
+        
+        if sender == forgotUserNameButton {
+            titleToSet = "Forgot User Name"
+        } else if sender == forgotPasswordButton {
+            titleToSet = "Forgot Password"
+        } else {
+            titleToSet = userNameTextField.text
+        }
+        
+        segue.destination.navigationItem.title = titleToSet
+    }
 }
 
